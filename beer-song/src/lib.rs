45b3b9a67022f627e322f5ae500151fb0c64c45f
Julia Naomi <jnaomi@thoughtworks.com>
2017-07-04 20:27:01 -0300

    adds first test for 0

---
+++ beer-song/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn verse(n: i32) -> String {
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string()
+}
+
+pub fn sing(start: i32, end: i32) -> String {
+    "".to_string()
+}
