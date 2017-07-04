2266e8941bc4b701f6f4d13a56b15e96cbac7f96
Julia Naomi <jnaomi@thoughtworks.com>
2017-07-04 20:47:38 -0300

    failed test...

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -3,10 +3,17 @@ pub fn verse(beers: i32) -> String {
         0 => "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string(),
         1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
         2 => "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string(),
-        num => "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n".to_string(),
-    }
+        n if n > 2 && n <= 99 =>
+            format!(
+                "{n} bottles of beer on the wall, {n} bottles of beer.\n\
+                 Take one down and pass it around, {n_1} bottles of beer on the wall.\n",
+                n=n,
+                n_1=n - 1),
+        _ =>
+            panic!(),
+        }
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n".to_string()
+    (start..end).map(|n| verse(n)).collect::<Vec<_>>().join("\n")
 }
--- beer-song/tests/beer-song.rs
+++ beer-song/tests/beer-song.rs
@@ -26,7 +26,6 @@ fn test_song_8_6() {
 }
 
 #[test]
-#[ignore]
 fn test_song_3_0() {
     assert_eq!(beer::sing(3, 0), "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
 }
