0740c46b498790e745286a15e7120e68df079e2d
Julia Naomi <jnaomi@thoughtworks.com>
2017-07-04 20:39:18 -0300

    last test standing

--- beer-song/src/lib.rs
+++ beer-song/src/lib.rs
@@ -8,5 +8,5 @@ pub fn verse(beers: i32) -> String {
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    "".to_string()
+    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n".to_string()
 }
