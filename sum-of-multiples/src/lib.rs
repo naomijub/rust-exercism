dfdc3816f2b7aa234073cfa037b02821830a104f
Julia Naomi <jnaomi@thoughtworks.com>
2017-07-08 00:18:47 -0300

    adds test

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn sum_of_multiples(num: u32, vec: &[u32]) -> u32 {
-    0
+    let mut sum :u32 = 0;
+
+    for &v in vec {
+        if v < num {
+            sum += v;
+        }
+    }
+    sum
 }
