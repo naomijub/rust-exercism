3d6e548caf9ab86c337aa1b84a730cbb629e4576
naomijub <jnaomi@thoughtworks.com>
2019-06-25 14:22:58 -0300

    Fixes variable name

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -7,12 +7,12 @@ pub fn build_proverb(list: &[&str]) -> String {
 }
 
 fn proverb_generator(list: &[&str]) -> String {
-  let mut counter = 1;
+  let mut next_item_counter = 1;
   let items_to_take = list.clone().len() - 1;
 
   let all_items = list.clone().into_iter().map(move |item| {
-    let next = list[counter];
-    if counter + 1 < list.len() { counter += 1}
+    let next = list[next_item_counter];
+    if next_item_counter + 1 < list.len() { next_item_counter += 1}
     format!("For want of a {} the {} was lost.\n", item, next)
   }).take(items_to_take).collect::<String>();
 
