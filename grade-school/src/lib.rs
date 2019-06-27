f062c5d11eb658a2eccbe9e1626b9686fe1c8b30
naomijub <jnaomi@thoughtworks.com>
2019-06-27 16:12:55 -0300

    Hate that sort and dedup do not return a vector and just mutate one :(. Very unfuctional

---
+++ grade-school/src/lib.rs
@@ -0,0 +1,30 @@
+pub struct School {
+    grades: Vec<Student>
+}
+
+struct Student(u32,String);
+
+impl School {
+    pub fn new() -> School {
+        School{grades: vec![]}
+    }
+
+    pub fn add(&mut self, grade: u32, student: &str) {
+        self.grades.push(Student(grade,student.to_string()));
+    }
+
+    pub fn grades(&self) -> Vec<u32> {
+        let mut grades = self.grades.iter().map(|s| s.0).collect::<Vec<u32>>().to_vec();
+        grades.dedup();
+        grades.sort();
+        grades
+    }
+
+    // If grade returned an `Option<&Vec<String>>`,
+    // the internal implementation would be forced to keep a `Vec<String>` to lend out.
+    // By returning an owned vector instead,
+    // the internal implementation is free to use whatever it chooses.
+    pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
+        None
+    }
+}
