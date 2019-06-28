1490dacee78dbe617b17039efa2498428eadd228
naomijub <jnaomi@thoughtworks.com>
2019-06-27 23:12:50 -0300

    Chunks str with windows

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -5,7 +5,7 @@ pub enum Error {
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    let mut digits: Vec<u64> = string_digits
+    let digits: Vec<u64> = string_digits
             .split("")
             .map(|s| s.parse())
             .filter(|s| match s {
@@ -14,9 +14,14 @@ pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
             })
             .map(|s| s.unwrap())
             .collect();
-    digits.sort();
-    digits.reverse();
-    digits.truncate(span);
 
-    Ok(digits.iter().fold(1,|acc, x| acc * x))
+    let mut str_chunks = digits
+                    .windows(span)
+                    .map(|x| x.to_vec())
+                    .map(|i| i.iter().fold(1,|acc, x| acc * x))
+                    .collect::<Vec<u64>>();
+    str_chunks.sort();
+    str_chunks.reverse();
+
+    Ok(str_chunks.first().unwrap().to_owned())
 }
