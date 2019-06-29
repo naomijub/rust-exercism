a6d9eb881052bd48a85b695e5399e74ff7d67d77
naomijub <jnaomi@thoughtworks.com>
2019-06-28 23:21:37 -0300

    Used string instead of char

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -1,10 +1,17 @@
 #[derive(Debug, PartialEq)]
 pub enum Error {
     SpanTooLong,
-    InvalidDigit(char),
+    InvalidDigit(String),
 }
 
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
+    if span > string_digits.len() { return Err(Error::SpanTooLong); }
+    else if span == 0 { return Ok(1u64); }
+    else if string_digits.matches(char::is_alphabetic).collect::<Vec<&str>>().len() > 0 {
+        let digit = string_digits.matches(char::is_alphabetic).collect::<Vec<&str>>();
+        return Err(Error::InvalidDigit(digit.first().unwrap().to_owned().to_string()));
+    }
+
     let digits: Vec<u64> = string_digits
             .split("")
             .map(|s| s.parse())
