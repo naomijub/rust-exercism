56e8be9697751c671ce8bf057a6d172a132211d5
Julia Naomi <jnboeira@outlook.com>
2020-06-30 20:21:57 -0300

    sum of multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,28 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let filteres_factors = factors.iter()
-            .filter(|c| c.to_owned() > &0u32)
-            .map(|c| c.to_owned())
-            .collect::<Vec<u32>>();
-    if filteres_factors.len() == 0
-        { return 0; }
-    else if filteres_factors.first().unwrap() > &limit 
-        { return 0; }
-    else {
-        let mut all_factors = filteres_factors.iter()
-            .filter(|v| v.to_owned() < &limit)
-            .map(|v| {
-                let aux_vec: Vec<u32> = (0..limit).collect();
-                aux_vec.iter()
-                    .filter(|s| s.to_owned() % v == 0u32)
-                    .map(|s| s.to_owned())
-                    .collect::<Vec<u32>>()
-            })
-            .collect::<Vec<Vec<u32>>>()
-            .into_iter()
-            .flatten()
-            .collect::<Vec<u32>>();
-        all_factors.sort();
-        all_factors.dedup();
-        all_factors.iter().fold(0,|acc, v| &acc + v.to_owned())
-    }
+    (1..limit)
+        .filter(|number| factors.iter().filter(|x| **x > 0).any(|x| number % x == 0))
+        .sum()
 }
