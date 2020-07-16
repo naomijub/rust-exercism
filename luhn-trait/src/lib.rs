a240a2fd6a74ce633aeeedd6afe8df90c8e1288a
Julia Naomi <jnboeira@outlook.com>
2020-07-15 21:56:32 -0300

    luhn trait

---
+++ luhn-trait/src/lib.rs
@@ -0,0 +1,30 @@
+pub trait Luhn: ToString {
+    fn valid_luhn(&self) -> bool {
+        let code = self.to_string();
+
+        if code.trim().len() <= 1 {
+            return false;
+        }
+    
+        let num_val = code
+            .replace(" ", "")
+            .chars()
+            .rev()
+            .map(|ch| ch.to_digit(10))
+            .enumerate()
+            .try_fold(0u32, |acc, (i, n)| 
+                match (i % 2, n) {
+                    (0, Some(v)) => Some(acc + v),
+                    (1, Some(v)) if v == 9 => Some(acc + v),
+                    (1, Some(v)) => Some(acc + ((v * 2) % 9)),
+                    _ => None
+                });
+    
+        match num_val {
+            Some(v) => v % 10 == 0,
+            None =>  false
+        }
+    }
+}
+
+impl<T: ToString> Luhn for T {}
